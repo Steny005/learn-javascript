9e81efe44ad5bead7b61653406a07c9ba35fafc5
Steny005 <stenytr05@gmail.com>
2025-09-02 11:18:32 +0530

    typeconversion in js

---
+++ typeconversion.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = "23";
+let word= "ghiya"
+console.log(a+b); // strings are added s such
+console.log(a-b); // converted to numbers then oprartions are done
+console.log(a*b);
+console.log(a%b);
+console.log (typeof word); //gives type
+console.log(a + word);
+console.log(a - word); //NaN not a number
+
+ 
+
